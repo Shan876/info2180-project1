ca482ba38ec3bb75d11efd9f5ca2d0f33e93231f
Shandene Gray <shandenegray20@gmail.com>
2020-11-01 21:13:17 -0500

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-/* Add your JavaScript to this file */
+function SubscriptionMsg() {
+    if (document.getElementById("email").innerHTML.length == 0) {
+        var message = document.getElementsByClassName('message')[0];
+        message.innerHTML += "Please enter a valid email address";}
+    else {
+        var message = document.getElementsByClassName('message')[0];
+        var newemail =document.getElementById("email").innerHTML;
+        message.innerHTML += "Thank you! Your email address ${newmail} has been added to our mailing list!";
+    }
+      }
